dc6a3dccc502d62650e3cde2c62fc0f080d6c786
Petrus <petrusheya@gmail.com>
2024-03-03 13:25:38 +0200

    Added Search component to community tab

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -2,6 +2,7 @@
 import Image from "next/image";
 import aston from '../../public/hero-images/aston.webp';
 import grim from '../../public/hero-images/grim.png';
+import { Input } from "@/components/ui/input"
 import {Select, SelectContent, SelectGroup, SelectItem, SelectLabel, SelectTrigger, SelectValue,} from "@/components/ui/select"
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
 import { Button } from "@/components/ui/button";
@@ -127,7 +128,11 @@ export default function dashboard() {
                                 />
                             </div>
                         </TabsContent>
-                        <TabsContent value="community">
+                        <TabsContent value="community" className="flex flex-col gap-4 items-center">
+                            <div className="w-full px-2 relative">
+                                <Input type="search" className="pl-10 text-white bg-neutral-950" placeholder="Search for Inspiration"/>
+                                <svg className="absolute top-1/2 -translate-y-1/2 left-4 fill-white" xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24"><path d="M784-120 532-372q-30 24-69 38t-83 14q-109 0-184.5-75.5T120-580q0-109 75.5-184.5T380-840q109 0 184.5 75.5T640-580q0 44-14 83t-38 69l252 252-56 56ZM380-400q75 0 127.5-52.5T560-580q0-75-52.5-127.5T380-760q-75 0-127.5 52.5T200-580q0 75 52.5 127.5T380-400Z"/></svg>
+                            </div>
                             <div className="h-[50%] grid grid-cols-2 grid-flow-row justify-center place-items-center gap-5 w-full px-2 ">
                                 <ProjectCard
                                     src={aston}
