672e6cf69b358aea41521415b9ad71e3881b8b0a
Petrus <petrusheya@gmail.com>
2024-03-09 20:14:39 +0200

    Community page done

--- app/dashboard/creations/page.jsx
+++ app/dashboard/creations/page.jsx
@@ -7,9 +7,10 @@ import styles from '../../../components/components.module.css'
 
 const Page = () => {
   return (
-    <section className='flex flex-col gap-2 items-start overflow-auto w-full h-full pb-2 relative '>
-        <div className='w-full flex items-center justify-end sticky top-0 left-0 bg-neutral-950 p-4  '>
-          <svg className="fill-white bg-black rounded-sm p-1 hover:fill-black hover:bg-white inline size-8 cursor-pointer" xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960" ><path d="M120-120v-200h80v120h120v80H120Zm520 0v-80h120v-120h80v200H640ZM120-640v-200h200v80H200v120h-80Zm640 0v-120H640v-80h200v200h-80Z"/></svg>
+    <section className='flex flex-col gap-2 items-center p-2 overflow-auto w-full h-full relative '>
+        <div className='w-full flex items-center justify-between sticky top-0 left-0 bg-neutral-950 p-4  '>
+          <p className='text-white font-semibold text-2xl tracking-tighter'>Your Artworks</p>
+          <svg className="fill-white bg-none rounded-sm p-1 hover:fill-white hover:bg-black inline size-8 cursor-pointer" xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960" ><path d="M120-120v-200h80v120h120v80H120Zm520 0v-80h120v-120h80v200H640ZM120-640v-200h200v80H200v120h-80Zm640 0v-120H640v-80h200v200h-80Z"/></svg>
         </div>
         <div className={`${styles.feedgrid} w-full`}>
             <ProjectCard src={img1}/>
--- components/ProjectCard.jsx
+++ components/ProjectCard.jsx
@@ -1,20 +1,13 @@
 import Image from 'next/image'
 import styles from "./components.module.css"
 import {Popover, PopoverContent, PopoverTrigger} from "@/components/ui/popover"
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuLabel,
-  DropdownMenuSeparator,
-  DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu"
+import { DropdownMenu, DropdownMenuContent, DropdownMenuTrigger,} from "@/components/ui/dropdown-menu"
 
 
 
 const ProjectCard = ({src}) => {
   return (
-    <div className={`flex items-center relative justify-between w-[12rem] h-auto cursor-pointer`}>
+    <div className={`flex items-center relative justify-between w-[15rem] h-auto cursor-pointer`}>
       <Image
       src={src}
       className="w-full h-full object-cover hover:zoom-in-50"
@@ -25,10 +18,6 @@ const ProjectCard = ({src}) => {
         </DropdownMenuTrigger>
         <DropdownMenuContent className="bg-black w-40">
           <div className="w-full bg-black flex flex-col gap-2">
-            <div className="hover:bg-red-600 text-white font-medium flex justify-between w-full items-center p-2 rounded-md cursor-pointer">
-              <p >Delete</p>
-              <svg className='fill-white size-5' xmlns="http://www.w3.org/2000/svg"  viewBox="0 -960 960 960" ><path d="M280-120q-33 0-56.5-23.5T200-200v-520h-40v-80h200v-40h240v40h200v80h-40v520q0 33-23.5 56.5T680-120H280Zm400-600H280v520h400v-520ZM360-280h80v-360h-80v360Zm160 0h80v-360h-80v360ZM280-720v520-520Z"/></svg>
-            </div>
             <div className="hover:bg-red-600 text-white font-medium flex justify-between w-full items-center p-2 rounded-md cursor-pointer">
               <p >Delete</p>
               <svg className='fill-white size-5' xmlns="http://www.w3.org/2000/svg"  viewBox="0 -960 960 960" ><path d="M280-120q-33 0-56.5-23.5T200-200v-520h-40v-80h200v-40h240v40h200v80h-40v520q0 33-23.5 56.5T680-120H280Zm400-600H280v520h400v-520ZM360-280h80v-360h-80v360Zm160 0h80v-360h-80v360ZM280-720v520-520Z"/></svg>
