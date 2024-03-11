d07e2a6e72b3ffd0176f427b9558a515358577ec
Petrus <petrusheya@gmail.com>
2024-03-11 20:27:52 +0200

    Seperated carousel into its own component

--- app/dashboard/layout.jsx
+++ app/dashboard/layout.jsx
@@ -1,98 +1,36 @@
-"use client"
-import React, {useState, useEffect, useRef} from "react";
-import Image from "next/image";
+
 import aston from '../../public/hero-images/aston.webp';
 import grim from '../../public/hero-images/grim.png';
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
-import {ResizableHandle, ResizablePanel, ResizablePanelGroup, } from "@/components/ui/resizable"
-import {getPanelElement} from "react-resizable-panels"
 import { ScrollArea, ScrollBar } from "@/components/ui/scroll-area" 
-import { Carousel, CarouselContent, CarouselItem, CarouselNext, CarouselPrevious, } from "@/components/ui/carousel"
 import Navigation from "@/components/Navigation";
-import ImageCarousel from "@/components/ImageCarousel";
 import ActionBtn from "@/components/ActionBtn";
 import { usePathname } from 'next/navigation'
+import CarouselSection from '@/components/CarouselSection';
 
 
 const layout = ({children}) => {
-    const pathName = usePathname()
-
     const urls = [aston, grim, aston, grim, aston, grim]
-    const panelRefs = useRef()
-
-    // useEffect(() => {
-    //     const leftPanelElement = getPanelElement("left-panel");
-    //     const rightPanelElement = getPanelElement("right-panel");
-    //     // console.log(rightPanelElement.gSizegit add .Id())
-        
-    
-    //     // If you want to, you can store them in a ref to pass around
-    //     panelRefs.current = {
-    //       leftPanelElement,
-    //       rightPanelElement,
-    //     };
-    // }, []);
-    
-    
-    
-    // const [prompt, setPrompt] = useState('')
-    // const [urls, setUrls] = useState([])
-
-    // const createPrediction = async()=>{
-    //     const response = await fetch('/api/create-prediction', {body: JSON.stringify(prompt), method: "POST"})
-    //     const predictionUrls = await response.json()
-    //     console.log("Url Array:", predictionUrls.urls.output)
-    //     setUrls(predictionUrls.urls.output)    
-    // }
-
-    // const updatePrompt = (e)=> {
-    //     setPrompt(e.target.value)  
-    // }
-
-    // const setAspectRatio = (aspect) =>{
-
-    // }
-  
   return (
     <main className="w-full h-screen flex flex-col bg-neutral-950  relative">
-        
         <nav className="flex justify-end items-center  border-gray-500 border-b h-[10%] p-2">
             <div className="flex gap-2 items-center">
                 <Avatar className="bg-black text-white font-semibold uppercase">
                     <AvatarImage src={aston} />
                     <AvatarFallback className="bg-black">PS</AvatarFallback>
                 </Avatar>
-
                 <ActionBtn>Sign Out</ActionBtn>
             </div>    
         </nav>
-        
-
-        <div   className=" h-[90%] flex justify-between items-center relative overflow-auto">
-            <Navigation currentUrl={pathName}/>
-
+        <div className=" h-[90%] flex justify-between items-center relative overflow-auto">
+            <Navigation/>
             <div className="flex-1 h-full relative flex flex-col items-center bg-neutral-950 border-l-gray-500 border-l text-white">
                 <ScrollArea className="h-full w-full">
                     {children}
                 </ScrollArea>
             </div>
-
-            {/* <ResizableHandle withHandle/> */}
-
-            <div className="flex-1 p-10 h-full flex flex-col gap-4 justify-center items-center relative bg-black bg-dot-white/[0.2] rounded-md">
-                    <Carousel className="p-10 w-[90%]">
-                        <CarouselContent className="w-full">
-                            {urls?.map((url)=>(
-                                <ImageCarousel src={url}/>
-                            ))}
-                        </CarouselContent>
-                        <CarouselPrevious />
-                        <CarouselNext />
-                    </Carousel>
-            </div>
-
+            <CarouselSection urls={urls}/>
         </div>
-        
     </main>
   )
 }
---
+++ components/CarouselSection.jsx
@@ -0,0 +1,21 @@
+import { Carousel, CarouselContent, CarouselItem, CarouselNext, CarouselPrevious, } from "./ui/carousel"
+import ImageCarousel from "./ImageCarousel";
+
+
+const CarouselSection = ({urls}) => {
+  return (
+    <div className="flex-1 hidden p-10 h-full flex-col gap-4 justify-center items-center relative bg-black bg-dot-white/[0.2] rounded-md">
+        <Carousel className="p-10 w-[90%]">
+            <CarouselContent className="w-full">
+                {urls?.map((url)=>(
+                    <ImageCarousel src={url}/>
+                ))}
+            </CarouselContent>
+            <CarouselPrevious />
+            <CarouselNext />
+        </Carousel>
+    </div>  
+  )
+}
+
+export default CarouselSection
