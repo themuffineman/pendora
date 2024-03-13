b0a38efb0cb212250fb684ce4ce68b8d0c6426a2
Petrus <petrusheya@gmail.com>
2024-03-13 11:46:17 +0200

    Refactored aspect ratio component

--- components/AspectRatio.jsx
+++ components/AspectRatio.jsx
@@ -1,9 +1,30 @@
+import { useContext } from 'react';
+import config from '../config';
 
-const AspectRatio = ({aspectToDisplay, selectedAspectRatio, changeAspectRatio}) => {
+const AspectRatio = () => {
+  const {aspectRatio, setAspectRatio} = useContext(ImageGenOptions)
+
+  const changeAspectRatio = (aspect)=>{
+    switch (aspect){
+        case "square":
+            setAspectRatio(config.aspect_ratios[0])
+            break;
+        case "portrait":
+            setAspectRatio(config.aspect_ratios[1])
+    }
+  }
   return (
-    <div className={`${selectedAspectRatio === aspectToDisplay && 'ring-2 ring-pendora-yellow'}  capitalize p-2 w-full font-medium rounded-md text-white text-center bg-neutral-700 cursor-pointer hover:bg-neutral-800`} onClick={() => changeAspectRatio(aspectToDisplay)}>
-        {aspectToDisplay}
+
+    <div className="grid grid-cols-2 grid-flow-row gap-2">
+      {config.aspect_ratios.map((aspect)=>(
+        <div className={`${aspectRatio.title === aspect.title && 'ring-2 ring-pendora-yellow'}  capitalize p-2 w-full font-medium rounded-md text-white text-center bg-neutral-700 cursor-pointer hover:bg-neutral-800`} onClick={() => changeAspectRatio(aspect.title)}>
+           {aspect.title}
+        </div>
+      ))}
     </div>
+   
+
+    // <AspectRatio aspectToDisplay={aspect.title} changeAspectRatio={changeAspectRatio} selectedAspectRatio={aspectRatio.title} />
   )
 }
 
