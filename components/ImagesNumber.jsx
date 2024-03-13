252b291670ac4fc589346cd7346d03b07c0add7e
Petrus <petrusheya@gmail.com>
2024-03-13 12:27:50 +0200

    refctored component into prompt area

--- components/ImagesNumber.jsx
+++ components/ImagesNumber.jsx
@@ -1,5 +1,9 @@
+"use client"
+import { useContext } from "react"
 
-const ImagesNumber = ({number, imagesNumber, setImagesNumber}) => {
+
+const ImagesNumber = ({number}) => {
+  const {imagesNumber, setImagesNumber} = useContext(ImageGenOptions)
   return (
     <div className={`${imagesNumber === number && 'ring-2 ring-pendora-yellow'}  p-2 size-12 font-medium rounded-md text-white text-center bg-neutral-700 cursor-pointer hover:bg-neutral-800`} onClick={()=>{setImagesNumber(number)}}>
         {number}
---
+++ components/PromptArea.jsx
@@ -0,0 +1,25 @@
+"use client"
+import React, { useContext, useState } from 'react'
+import ActionBtn from "./ActionBtn";
+import { Switch } from "@/components/ui/switch";
+
+
+const PromptArea = () => {
+    const { setPrompt, setNegativePrompt} = useContext(ImageGenOptions)
+    const [isNegPrompt, setIsNegPrompt] = useState(false)
+
+
+  return (
+    <div>
+        <textarea onChange={(e)=> {setPrompt(e.target.value)}} rows="5" cols="43" name="prompt" id="prompt" className="text-white bg-black rounded-md focus:outline-pendora-yellow outline-1 p-2"/>
+            <div className="flex w-full justify-between items-center">
+                <label htmlFor="negative prompt" className="text-white text-lg tracking-tighter font-medium">Negative Prompt</label>
+                <Switch id="negative prompt" checked={isNegPrompt} onClick={()=> setIsNegPrompt(prev => !prev)} />
+            </div>
+        <textarea onChange={(e)=> {setNegativePrompt(e.target.value)}} rows="5" cols="43" name="negative prompt" id="negative prompt" className={`text-white ${!isNegPrompt? 'hidden': 'block'} bg-black rounded-md focus:outline-pendora-yellow outline-1 p-2`}/>
+        <ActionBtn className="mt-4" >Generate Image</ActionBtn>
+    </div>
+  )
+}
+
+export default PromptArea
