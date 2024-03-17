65191f1c101b0f890d43b3429d4a0ace9c34515b
Petrus <petrusheya@gmail.com>
2024-03-17 15:55:18 +0200

    Reverted back to proteas model

--- app/api/create-prediction/route.js
+++ app/api/create-prediction/route.js
@@ -1,5 +1,6 @@
 
 export const POST = async (req) =>{
+  
   const fetchData = await req.json()
 
   console.log("This is the fetch data:", fetchData)
@@ -11,34 +12,34 @@ export const POST = async (req) =>{
         'Content-Type': 'application/json'
       },
       body: JSON.stringify({
-        "version": "0a1710e0187b01a255302738ca0158ff02a22f4638679533e111082f9dd1b615",
+        "version": "06775cd262843edbde5abab958abdbb65a0a6b58ca301c9fd78fa55c775fc019",
         "input": {
           "width": fetchData.width,
           "height": fetchData.height,
           "prompt": fetchData.prompt,
-          "scheduler": "K_EULER",
+          "scheduler": "KarrasDPM",
           "num_outputs": fetchData.quantity,
-          "guidance_scale": 2,
+          "guidance_scale": 7.5,
           "apply_watermark": true,
           "negative_prompt": "worst quality",
-          // "prompt_strength": 0.8,
-          "num_inference_steps": 7
+          "prompt_strength": 0.8,
+          "num_inference_steps": 20
         }
       })
   };
     
     
     try {
-      const prediction = await fetch('https://api.replicate.com/v1/predictions', requestOptions)
-      const predictionJSON = await prediction.json()
-      console.log("This is the predictionJSON", predictionJSON)
-      const output = await fetch('/api/get-prediction', {method:"POST", body: JSON.stringify(predictionJSON.id)})
-      const outputJSON = await output.json()
-      console.log('The prediction output from create prediction:', outputJSON )
+      const predictionJSON = await fetch('https://api.replicate.com/v1/predictions', requestOptions)
+      const prediction = await predictionJSON.json()
+      console.log("This is the prediction Object", prediction)
+      const outputJSON = await fetch('/api/get-prediction', {method:"POST", body: JSON.stringify(prediction.id)})
+      const output = await outputJSON.json()
+      console.log('The prediction output from create prediction:', output)
 
-      return Response.json({urls: outputJSON.output}, {status: 201})   
+      return Response.json({urls: output.output}, {status: 201})   
     } catch (error) {
-      return Response.json({error: `❌Error on creating prediction: ${error}`}, {status: 500})
+      return Response.error(`❌Error on creating prediction: ${error}`, {status: 500})
     }
 
 
--- components/ImageCarousel.jsx
+++ components/ImageCarousel.jsx
@@ -1,16 +1,18 @@
 "use client"
-import { useContext } from "react"
+import { useContext, useState } from "react"
 import { ImageGenOptions } from '@/app/global-state/ImageGeneration';
 import { Card, CardContent } from "./ui/card"
 import { CarouselItem } from "./ui/carousel"
 import styles from './components.module.css'
 import Image from "next/image"
+import CardSkeleton from "./CardSkeleton";
 
 
 
 const ImageCarousel = ({src, index}) => {
 
-  const {hdr, upscaleIntensity, prompt, setLoadingImages, setImageUrls} = useContext(ImageGenOptions)
+  const {hdr, upscaleIntensity, prompt, setImageUrls} = useContext(ImageGenOptions)
+  const [upscaling, setUpscaling] = useState(false)
   const calcHdr = hdr/100
   const calcIntentsity = upscaleIntensity/100
 
@@ -23,19 +25,26 @@ const ImageCarousel = ({src, index}) => {
 
   const createUpscale = async ()=>{
     try {
-        // setLoadingImages(true)
+        setUpscaling(true)
         const imageJSON = await fetch('/api/create-upscale', {method: "POST", body: JSON.stringify(upscaleFetchBody)})
         const image = await imageJSON.json()
-        // setLoadingImages(false)
-        console.log("here are upscale urls:", image.urls)
-        setImageUrls((prev)=> prev[index] = image.urls ) 
+        setUpscaling(false)
+        console.log("here are upscale urls:", image.url)
+        setImageUrls((prev)=> {
+          arrayCopy = [...prev]
+          arrayCopy[index] = image.url
+          return arrayCopy;
+        })
     } catch (error) {
         
     }
   }
 
   return (
+    
+  upscaling? (<CardSkeleton/>) : (
     <CarouselItem className="">
+
         <Card>
             <CardContent className={`flex aspect-square items-center justify-center p-0 relative ${styles.gen_image}`}>
                 <img
@@ -46,13 +55,13 @@ const ImageCarousel = ({src, index}) => {
                   <span onClick={createUpscale} className=" text-white text-sm bg-black rounded-sm w-max p-1 cursor-pointer hover:bg-neutral-700">
                     Upscale
                   </span>
-                  <a href={src} download={'image-hello.png'}>
+                  <a href={src} download='image.png'>
                     <svg className=" fill-white bg-black rounded-sm size-7 cursor-pointer hover:bg-neutral-700" xmlns="http://www.w3.org/2000/svg"  viewBox="0 -960 960 960" ><path d="M480-320 280-520l56-58 104 104v-326h80v326l104-104 56 58-200 200ZM240-160q-33 0-56.5-23.5T160-240v-120h80v120h480v-120h80v120q0 33-23.5 56.5T720-160H240Z"/></svg>
                   </a>
                 </span>
             </CardContent>
         </Card>
-    </CarouselItem>
+    </CarouselItem>)
   )
 }
 
