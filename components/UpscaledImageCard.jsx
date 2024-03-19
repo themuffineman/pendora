6ff6ce89d41e40dc5caae3a260a5b12ea654406a
Petrus <petrusheya@gmail.com>
2024-03-19 04:47:04 +0200

    Added line and button

--- components/UpscaledImageCard.jsx
+++ components/UpscaledImageCard.jsx
@@ -9,11 +9,15 @@ import img1 from '../public/hero-images/img1.webp'
 const UpscaledImageCard = () => {
     
     const afterImg = useRef(null)
+    const line = useRef(null)
+    const icon = useRef(null)
 
     const updateSlider = (event)=>{
-        console.log('these are the styles:', afterImg.current.style)
-        console.log('these are the current:', afterImg.current)
         afterImg.current.style.clipPath = ` inset(0 0 0 ${event.target.value}%)`
+        line.current.style.left = `${event.target.value}%)`
+        console.log('clip',  afterImg.current.style.left)
+        icon.current.style.left = ` ${event.target.value}%)`
+        console.log('icon left',  icon.current.style.left )
 
     }
     
@@ -40,12 +44,12 @@ const UpscaledImageCard = () => {
             value={50} 
             step={1}
             onInput={updateSlider}
-            className={`absolute inset-0 cursor-pointer opacity-0 size-full ${styles.slider}`}
+            className={`absolute inset-0 cursor-pointer opacity-0 size-full`}
             aria-label="Percentage of Upscaled phot shown"/>
-            {/* <div className={`slider-line absolute inset-0 w-[2px] pointer-events-none h-full bg-white z-10 left-1/2 -translate-x-1/2`}></div>
-            <div className={`slider-button absolute bg-white fill-black rounded-full p-1 grid place-items-center top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2  pointer-events-none z-50 shadow-lg`} aria-hidden="true">
+            <div ref={line} className={`${styles.slider_line}`}></div>
+            <div ref={icon} className={`${styles.slider_button}`} aria-hidden="true">
                 <svg className="" xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" viewBox="0 0 256 256"><path d="M136,40V216a8,8,0,0,1-16,0V40a8,8,0,0,1,16,0ZM96,120H35.31l18.35-18.34A8,8,0,0,0,42.34,90.34l-32,32a8,8,0,0,0,0,11.32l32,32a8,8,0,0,0,11.32-11.32L35.31,136H96a8,8,0,0,0,0-16Zm149.66,2.34-32-32a8,8,0,0,0-11.32,11.32L220.69,120H160a8,8,0,0,0,0,16h60.69l-18.35,18.34a8,8,0,0,0,11.32,11.32l32-32A8,8,0,0,0,245.66,122.34Z"></path></svg>
-            </div> */}
+            </div>
         </div>
     </CarouselItem>
   )
